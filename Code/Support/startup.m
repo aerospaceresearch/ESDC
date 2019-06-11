a9b5713e8afc426ff362a8b99047f42baa935b68
AstroEngy <ehresmann.manfred@gmail.com>
2019-06-11 14:22:18 +0200

    Add files via upload
    Added License Display at Startup and CLI flushing

--- Code/Support/startup.m
+++ Code/Support/startup.m
@@ -2,13 +2,35 @@ function [] = startup()
 clc;
 close all;
 disp('Evolutionary System Design Converger')
+disp('Institute of Space Systems')
+disp('University of Stuttgart')
+disp('Main author: Manfred Ehresmann')
+disp(' ')
+disp(' ')
+disp('MIT License')
+disp('')
+disp('Copyright (c) 2019 AerospaceResearch ')
+disp('Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:')
+disp('')
+disp('The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.')
+disp('')
+disp('THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.')
+disp(' ')
+disp(' ')
+disp(' ')
 disp('Start ESDC solver')
 disp(' ')
-disp('Institute of Space Systems')
-disp('Author: Manfred Ehresmann')
 disp(' ')
 disp('Loading Files ...')
 disp(' ')
 end
+
+
+
+
+
+
+
+
 
 
