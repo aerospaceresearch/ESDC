9a5ca82760058999559860ea0ce713c88aa2a700
AstroEngy <ehresmann.manfred@gmail.com>
2019-06-11 15:17:47 +0200

    Add files via upload
    Added function to assess total time of evolution

---
+++ Code/Analysis/evolution_time.m
@@ -0,0 +1,6 @@
+function evolution_time(t_1, t_2)
+%produces time difference in seconds  
+t_to_convergence = (t_2-t_1)*60*60*24;
+disp(sprintf('Total time for evolver convergence: %d s', t_to_convergence ))
+fflush(stdout);
+end
