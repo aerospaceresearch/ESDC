d936af08a6253be1bf263f1611a34caeb8b938f6
AstroEngy <ehresmann.manfred@gmail.com>
2019-06-11 15:57:54 +0200

    Add files via upload

--- Code/Analysis/evolution_time.m
+++ Code/Analysis/evolution_time.m
@@ -1,5 +1,5 @@
 function evolution_time(t_1, t_2)
-%produces time difference in seconds  
+%produces evolution time difference in seconds  
 t_to_convergence = (t_2-t_1)*60*60*24;
 disp(sprintf('Total time for evolver convergence: %d s', t_to_convergence ))
 fflush(stdout);
