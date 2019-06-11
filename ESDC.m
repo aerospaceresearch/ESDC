51b75af0b79fe872c138782616c985a0242b2461
AstroEngy <ehresmann.manfred@gmail.com>
2019-06-11 14:21:07 +0200

    Add files via upload
    Added cleaner CLI Output

--- ESDC.m
+++ ESDC.m
@@ -8,22 +8,25 @@ addpath('Code/Output')
 addpath('Code/Scaling')
 addpath('Code/Support')
 
-
 startup();
+fflush(stdout);
 
 update_scaling_model();
 
 [input db_data config] = input_processing();
 
-disp('Starting analysis ...');
+disp('Starting Evolution ...');
 disp(' ');
+fflush(stdout);
 
 t_1=now;
 evolution_data = evolver(input, db_data, config);
 t_2=now;
+t_to_convergence = (t_2-t_1)*60*60*24;
 
-dt=(t_2-t_1)*60*60*24
-n_gen_to_convergence = size(evolution_data,2)
+disp(sprintf('Total time for evolver convergence: %d s', t_to_convergence ))
+
+
 
 %disp('EP mass system scaling for reference points');
 %disp(' ');
