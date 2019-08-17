587f124dc6873ca88c83b47d56e59c16559340db
s-themis <themistos@auth.gr>
2019-08-17 11:46:15 +0300

    added bar graphs & compression for gif animations, adjusted axes limits

--- Code/Output/visualization.m
+++ Code/Output/visualization.m
@@ -46,6 +46,10 @@ function [] = visualize_input_case(evolution_data, input, plot_case, n_plot_case
   
   plot_case = set_visualization_lineages(evolution_data, plot_case, n_input_case);
   plot_case = add_min_max_values_for_continuous_dofs(plot_case, evolution_data, n_input_case);
+  plot_case = set_subsystems_fieldnames(plot_case, evolution_data, n_input_case);
+  plot_case = set_number_of_subsystems(plot_case);
+  plot_case = set_subsystems_line_colors(plot_case);
+  
   set_visualization_appearance(plot_case)
   
   if strcmp(get_plot_case_type(plot_case), "3d")
@@ -98,12 +102,7 @@ function [] = visualize_input_case(evolution_data, input, plot_case, n_plot_case
   
 end
 
-function num_gens = get_number_of_visualized_generations_per_lineage(graphics_handles)
-  num_lineages = numel(graphics_handles);
-  for n_lineage = 1:num_lineages
-    num_gens(n_lineage) = numel(graphics_handles{n_lineage});
-  end
-end  
+ 
 
 function [] = animate_and_save_visualization(plot_case, fig, graphics_handles)
   if is_animate_and_save_active(plot_case)
@@ -121,26 +120,27 @@ function [] = animate_and_save_visualization(plot_case, fig, graphics_handles)
 end
 
 function [] = animate_and_save_visualization_by_lineage(plot_case, fig, graphics_handles, filename_with_path)
-  [fps, delay_time, num_gens, max_gens, num_lineages] = get_parameters_for_animation(plot_case, graphics_handles);
-  initialize_animation_file(filename_with_path, fig, delay_time);
+  num_lineages = numel(graphics_handles);
+  num_gens = get_number_of_visualized_generations_per_lineage(graphics_handles);
+  initialize_animation_file(filename_with_path, fig, plot_case);
   for n_lineage = 1:num_lineages
-    num_gens = numel(graphics_handles{n_lineage});
-    for n_gen = 1:num_gens
+    for n_gen = 1:num_gens(n_lineage)
       if ~isempty(graphics_handles{n_lineage}{n_gen})
         num_graphics = numel(graphics_handles{n_lineage}{n_gen});
         for n_graphic = 1:num_graphics
           graphic_handle = graphics_handles{n_lineage}{n_gen}{n_graphic};
           show_graphic(graphic_handle);
         end
-        add_frame_to_animation_file(filename_with_path, fig, delay_time);
+        add_frame_to_animation_file(filename_with_path, fig, plot_case);
       end
     end
   end
 end
 
 function [] = animate_and_save_visualization_by_generation(plot_case, fig, graphics_handles, filename_with_path)
-  [fps, delay_time, num_gens, max_gens, num_lineages] = get_parameters_for_animation(plot_case, graphics_handles);
-  initialize_animation_file(filename_with_path, fig, delay_time);
+  num_lineages = numel(graphics_handles);
+  max_gens = get_max_visualized_generations(graphics_handles);
+  initialize_animation_file(filename_with_path, fig, plot_case);
   for n_gen = 1:max_gens
     for n_lineage = 1:num_lineages
       try
@@ -155,28 +155,49 @@ function [] = animate_and_save_visualization_by_generation(plot_case, fig, graph
         % nothing to add here, the use of try-catch is only to avoid out of bound errors
       end
     end
-    add_frame_to_animation_file(filename_with_path, fig, delay_time);
+    add_frame_to_animation_file(filename_with_path, fig, plot_case);
   end 
 end
 
-function [fps, delay_time, num_gens, max_gens, num_lineages] = get_parameters_for_animation(plot_case, graphics_handles)
-  fps = get_fps(plot_case);
-  delay_time = 1/fps;
-  num_gens = get_number_of_visualized_generations_per_lineage(graphics_handles);
-  max_gens = max(num_gens);
+function num_gens = get_number_of_visualized_generations_per_lineage(graphics_handles)
   num_lineages = numel(graphics_handles);
-end    
-
-function [] = initialize_animation_file(filename, fig, delay_time)
-  [A, map] = get_indexed_image(fig);
-  %imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',delay_time,'Compression','lzw');
-  imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',delay_time);
+  for n_lineage = 1:num_lineages
+    num_gens(n_lineage) = numel(graphics_handles{n_lineage});
+  end
 end
 
-function [] = add_frame_to_animation_file(filename, fig, delay_time)
+function max_gens = get_max_visualized_generations(graphics_handles)
+  num_gens = get_number_of_visualized_generations_per_lineage(graphics_handles);
+  max_gens = max(num_gens);
+end
+  
+function time_step = get_animation_time_step(plot_case)
+  fps = get_fps(plot_case);
+  time_step = 1/fps;
+end
+
+function bool = is_animation_compression_active(plot_case)
+  bool = plot_case.animate_and_save.compression;
+end
+
+function [] = initialize_animation_file(filename, fig, plot_case)
+  time_step = get_animation_time_step(plot_case);
   [A, map] = get_indexed_image(fig);
-  %imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',delay_time,'Compression','lzw');
-  imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',delay_time);
+  if is_animation_compression_active(plot_case)
+    imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',time_step,'Compression','lzw');
+  else
+    imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',time_step);
+  end
+end
+
+function [] = add_frame_to_animation_file(filename, fig, plot_case)
+  time_step = get_animation_time_step(plot_case);
+  [A, map] = get_indexed_image(fig);
+  if is_animation_compression_active(plot_case)
+    imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',time_step,'Compression','lzw');
+  else
+    imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',time_step);
+  end
 end
 
 function [A, map] = get_indexed_image(fig)
@@ -325,7 +346,11 @@ function [xlimits, ylimits, zlimits] = get_axes_limits_3d(plot_case)
   else
     xlimits = [plot_case.x.min_value, plot_case.x.max_value];
     ylimits = [plot_case.y.min_value, plot_case.y.max_value];
-    zlimits = [plot_case.z.min_value, plot_case.z.max_value];
+    if are_subsystems_bars_active(plot_case)
+      zlimits = [0, plot_case.z.max_value];
+    else
+      zlimits = [plot_case.z.min_value, plot_case.z.max_value];
+    end
   end
 end
 
@@ -335,7 +360,11 @@ function [xlimits, ylimits, zlimits] = get_axes_limits_2d(plot_case)
     ylimits = plot_case.appearance.custom_limits.ylim;
   else
     xlimits = [plot_case.x.min_value, plot_case.x.max_value];
-    ylimits = [plot_case.y.min_value, plot_case.y.max_value];
+    if are_subsystems_bars_active(plot_case)
+      ylimits = [0, plot_case.y.max_value];
+    else
+      ylimits = [plot_case.y.min_value, plot_case.y.max_value];
+    end
   end
 end
 
@@ -410,13 +439,13 @@ function [xdum, ydum] = get_dummy_point_2d(plot_case)
   ydum = mean(ylimits);
 end
 
-function [] = plot_dummy_line(plot_case, line_type, line_color)
+function [] = plot_dummy_line(plot_case, line_type, line_color, line_width)
   if strcmp(get_plot_case_type(plot_case), "3d")
     [xdum, ydum, zdum] = get_dummy_line_3d(plot_case);
-    obj = plot3(xdum, ydum, zdum, "linestyle", line_type, "color", line_color);
+    obj = plot3(xdum, ydum, zdum, "linestyle", line_type, "color", line_color, "linewidth", line_width);
   elseif strcmp(get_plot_case_type(plot_case), "2d")
     [xdum, ydum] = get_dummy_line_2d(plot_case);
-    obj = plot(xdum, ydum, "linestyle", line_type, "color", line_color);
+    obj = plot(xdum, ydum, "linestyle", line_type, "color", line_color, "linewidth", line_width);
   end
   set(obj, "visible", "off");
 end
@@ -436,9 +465,10 @@ function [] = plot_dummy_graphics(plot_case, plot_dof)
   if is_plot_dof_active(plot_case, plot_dof)
     if strcmp(plot_dof, "line_type")
       line_color = "k";
+      line_width = 0.5;
       for i = 1:numel(plot_case.(plot_dof).plot_values)
         line_type = plot_case.(plot_dof).plot_values{i};
-        plot_dummy_line(plot_case, line_type, line_color)
+        plot_dummy_line(plot_case, line_type, line_color, line_width)
       end
     elseif strcmp(plot_dof, "marker_type")
       marker_color = "k";
@@ -450,12 +480,23 @@ function [] = plot_dummy_graphics(plot_case, plot_dof)
       colors = lines;
       line_type = "-";
       line_color = colors(2,:);
-      plot_dummy_line(plot_case, line_type, line_color);
+      line_width = 0.5;
+      plot_dummy_line(plot_case, line_type, line_color, line_width);
     elseif strcmp(plot_dof, "marker_color")
       colors = lines;
       marker_type = "o";
       marker_color = colors(2,:);
       plot_dummy_point(plot_case, marker_type, marker_color)
+    elseif strcmp(plot_dof, "subsystems_bars")
+      %%%%%%
+      line_type = get_subsystems_line_type(plot_case);
+      line_colors = get_subsystems_line_colors(plot_case);
+      line_width = get_subsystems_line_width(plot_case);
+      num_subsystems = get_number_of_subsystems(plot_case);
+      for n_subsystem = 1:num_subsystems
+        line_color = line_colors(n_subsystem,:);
+        plot_dummy_line(plot_case, line_type, line_color, line_width)
+      end
     end
   end
 end
@@ -467,8 +508,19 @@ function dof_names = get_dof_names(plot_case, plot_dof)
     else
       dof_names = pot_case.(plot_dof).dof_values;
     end
+    if strcmp(plot_dof, "line_color") || strcmp(plot_dof, "marker_color")
+      dof_names = strcat(dof_names, sprintf(" [%.2f,%.2f]", plot_case.(plot_dof).min_value, plot_case.(plot_dof).max_value));
+    end
   else
     dof_names = {};
+  end
+end
+
+function subsystems_names = get_subsystems_names(plot_case)
+  if are_subsystems_bars_active(plot_case)
+    subsystems_names = get_subsystems_fieldnames(plot_case);
+  else
+    subsystems_fieldnames = {};
   end
 end
 
@@ -477,7 +529,9 @@ function legend_names = get_legend_names(plot_case)
   line_color_names = get_dof_names(plot_case, "line_color");
   marker_type_names = get_dof_names(plot_case, "marker_type");
   marker_color_names = get_dof_names(plot_case, "marker_color");
-  legend_names = [line_type_names, marker_type_names, line_color_names, marker_color_names];
+  %%%%%
+  subsystems_names = get_subsystems_names(plot_case);
+  legend_names = [line_type_names, marker_type_names, line_color_names, marker_color_names, subsystems_names];
 end
 
 function [] = set_plot_legend(plot_case)
@@ -486,6 +540,7 @@ function [] = set_plot_legend(plot_case)
     plot_dummy_graphics(plot_case, "marker_type")
     plot_dummy_graphics(plot_case, "line_color")
     plot_dummy_graphics(plot_case, "marker_color")
+    plot_dummy_graphics(plot_case, "subsystems_bars")
     legend_names = get_legend_names(plot_case);
     legend(legend_names)
   end
@@ -500,7 +555,6 @@ function [] = set_visualization_appearance(plot_case)
     set_viewing_angle(plot_case)
   end
 end
-
 
 function name = get_label_name(plot_case, plot_dof)
   if plot_case.appearance.custom_display_names.(plot_dof).active
@@ -560,8 +614,8 @@ function plot_case = add_min_max_values_for_continuous_dofs(plot_case, evolution
         continuous_plot_dof = potentially_continuous_plot_dof;
         dof = plot_case.(continuous_plot_dof).dof;
         [min_value, max_value] = get_visualization_min_max_dof_value(plot_case, evolution_data, n_input_case, dof);
-        plot_case.(potentially_continuous_plot_dof).min_value = min_value;
-        plot_case.(potentially_continuous_plot_dof).max_value = max_value;
+        plot_case.(continuous_plot_dof).min_value = min_value;
+        plot_case.(continuous_plot_dof).max_value = max_value;
       end
     end
   end      
@@ -791,31 +845,149 @@ function graphics_handles_seed_point = plot_seed_point_3d(plot_case, lineage, n_
   [x_sctr3, y_sctr3, z_sctr3, marker_type, marker_color] = get_scatter3_data(plot_case, lineage, n_gen);
   sizedata = plot_case.seed_points.sizedata;
   graphics_handles_seed_point{1} = scatter3(x_sctr3, y_sctr3, z_sctr3, "marker", marker_type, "markeredgecolor", marker_color,"sizedata",sizedata);
+  if are_subsystems_bars_active(plot_case)
+    graphics_handles = plot_bar_3d(plot_case, lineage, n_gen);
+    graphics_handles_seed_point = [graphics_handles_seed_point, graphics_handles];
+  end
 end
 
 function graphics_handles_seed_point = plot_seed_point_2d(plot_case, lineage, n_gen)
   [x_sctr, y_sctr, marker_type, marker_color] = get_scatter_data(plot_case, lineage, n_gen);
   sizedata = plot_case.seed_points.sizedata;
-  graphics_handles_seed_point{1} = scatter(x_sctr, y_sctr, "marker", marker_type, "markeredgecolor", marker_color,"sizedata",sizedata);
+  graphics_handles_seed_point = {};
+  graphic_handle = scatter(x_sctr, y_sctr, "marker", marker_type, "markeredgecolor", marker_color,"sizedata",sizedata);
+  graphics_handles_seed_point = [graphics_handles_seed_point, graphic_handle];
+  if are_subsystems_bars_active(plot_case)
+    graphics_handles = plot_bar_2d(plot_case, lineage, n_gen);
+    graphics_handles_seed_point = [graphics_handles_seed_point, graphics_handles];
+  end
 end
 
 function graphics_handles_n_gen = plot_generation_3d(plot_case, lineage, n_gen)  
   [x_plt3, y_plt3, z_plt3, line_type, line_color] = get_plot3_data(plot_case, lineage, n_gen);
-  graphics_handles_n_gen{1} = plot3(x_plt3, y_plt3, z_plt3, "linestyle", line_type, "color", line_color);
+  graphics_handles_n_gen = {};
+  graphic_handle = plot3(x_plt3, y_plt3, z_plt3, "linestyle", line_type, "color", line_color);
+  graphics_handles_n_gen = [graphics_handles_n_gen, graphic_handle];
   if is_scatter_active(plot_case)
     [x_sctr3, y_sctr3, z_sctr3, marker_type, marker_color] = get_scatter3_data(plot_case, lineage, n_gen);
-    graphics_handles_n_gen{2} = scatter3(x_sctr3, y_sctr3, z_sctr3, "marker", marker_type, "markeredgecolor", marker_color);
+    graphic_handle = scatter3(x_sctr3, y_sctr3, z_sctr3, "marker", marker_type, "markeredgecolor", marker_color);
+    graphics_handles_n_gen = [graphics_handles_n_gen, graphic_handle];
+  end
+  if are_subsystems_bars_active(plot_case)
+    graphics_handles = plot_bar_3d(plot_case, lineage, n_gen);
+    graphics_handles_n_gen = [graphics_handles_n_gen, graphics_handles];
   end
 end
 
-
-
 function graphics_handles_n_gen = plot_generation_2d(plot_case, lineage, n_gen)
   [x_plt, y_plt, line_type, line_color] = get_plot_data(plot_case, lineage, n_gen);
-  graphics_handles_n_gen{1} = plot(x_plt, y_plt, "linestyle", line_type, "color", line_color);
+  graphics_handles_n_gen = {};
+  graphic_handle = plot(x_plt, y_plt, "linestyle", line_type, "color", line_color);
+  graphics_handles_n_gen = [graphics_handles_n_gen, graphic_handle];
   if is_scatter_active(plot_case)
     [x_sctr, y_sctr, marker_type, marker_color] = get_scatter_data(plot_case, lineage, n_gen);
-    graphics_handles_n_gen{2} = scatter(x_sctr, y_sctr, "marker", marker_type, "markeredgecolor", marker_color);
+    graphic_handle = scatter(x_sctr, y_sctr, "marker", marker_type, "markeredgecolor", marker_color);
+    graphics_handles_n_gen = [graphics_handles_n_gen, graphic_handle];
+  end
+  if are_subsystems_bars_active(plot_case)
+    graphics_handles = plot_bar_2d(plot_case, lineage, n_gen);
+    graphics_handles_n_gen = [graphics_handles_n_gen, graphics_handles];
+  end
+end
+
+function bool = are_subsystems_bars_active(plot_case)
+  bool = plot_case.subsystems_bars.active;
+end
+
+function num_subsystems = get_number_of_subsystems(plot_case)
+  num_subsystems = plot_case.subsystems_bars.num_subsystems;
+end
+
+function subsystems_fieldnames = get_subsystems_fieldnames(plot_case)
+  subsystems_fieldnames = plot_case.subsystems_bars.subsystems_fieldnames;
+end
+
+function line_type = get_subsystems_line_type(plot_case)
+  line_type = plot_case.subsystems_bars.line_type;
+end
+
+function line_colors = get_subsystems_line_colors(plot_case)
+  line_colors = plot_case.subsystems_bars.line_colors;
+end
+
+function plot_case = set_subsystems_line_colors(plot_case)
+  colormap = plot_case.subsystems_bars.line_colors_colormap{1};
+  num_colors = get_number_of_subsystems(plot_case);
+  line_colors = eval(sprintf("%s(%d)", colormap, num_colors));
+  plot_case.subsystems_bars.line_colors = line_colors;
+end
+
+function line_width = get_subsystems_line_width(plot_case)
+  line_width = plot_case.subsystems_bars.line_width;
+end
+
+function container_field = get_container_field(plot_case)
+  container_field = plot_case.subsystems_bars.container_field;
+end
+
+function graphics_handles = plot_bar_2d(plot_case, lineage, n_gen)
+  graphics_handles = {};
+  num_subsystems = get_number_of_subsystems(plot_case);
+  subsystems_fieldnames = get_subsystems_fieldnames(plot_case);
+  container_field = get_container_field(plot_case);
+  line_type = get_subsystems_line_type(plot_case);
+  line_colors = get_subsystems_line_colors(plot_case);
+  line_width = get_subsystems_line_width(plot_case);
+  x_plt = [n_gen, n_gen];
+  prev_sum_mass_frac = 0;
+  sum_mass_frac = 0;
+  for n_subsystem = 1:num_subsystems
+    dof = {container_field, subsystems_fieldnames{n_subsystem}};
+    n_subsystem_mass_frac = get_dof_value(lineage, n_gen, dof);
+    sum_mass_frac = sum_mass_frac + n_subsystem_mass_frac;
+    y_plt = [prev_sum_mass_frac, sum_mass_frac];
+    graphic_handle = plot(x_plt, y_plt, "linestyle", line_type, "color", line_colors(n_subsystem,:), "linewidth", line_width);
+    graphics_handles = [graphics_handles, graphic_handle];
+    prev_sum_mass_frac = sum_mass_frac;
+  end
+end
+
+function plot_case = set_subsystems_fieldnames(plot_case, evolution_data, n_input_case)
+  container_field = get_container_field(plot_case);
+  n_gen = 1;
+  n_lineage = 1;
+  subsystems_fieldnames = fieldnames(evolution_data{n_gen}(n_input_case, n_lineage).(container_field));
+  subsystems_fieldnames(end) = [];
+  subsystems_fieldnames = subsystems_fieldnames';
+  plot_case.subsystems_bars.subsystems_fieldnames = subsystems_fieldnames;
+end
+
+function plot_case = set_number_of_subsystems(plot_case)
+  num_subsystems = numel(get_subsystems_fieldnames(plot_case));
+  plot_case.subsystems_bars.num_subsystems = num_subsystems;
+end
+
+function graphics_handles = plot_bar_3d(plot_case, lineage, n_gen)
+  graphics_handles = {};
+  num_subsystems = get_number_of_subsystems(plot_case);
+  subsystems_fieldnames = get_subsystems_fieldnames(plot_case);
+  container_field = get_container_field(plot_case);
+  line_type = get_subsystems_line_type(plot_case);
+  line_colors = get_subsystems_line_colors(plot_case);
+  line_width = get_subsystems_line_width(plot_case);
+  [x_sctr3, y_sctr3, dum_z_sctr3, dum_marker_type, dum_marker_color] = get_scatter3_data(plot_case, lineage, n_gen);
+  x_plt3 = [x_sctr3, x_sctr3];
+  y_plt3 = [y_sctr3, y_sctr3];
+  prev_sum_mass_frac = 0;
+  sum_mass_frac = 0;
+  for n_subsystem = 1:num_subsystems
+    dof = {container_field, subsystems_fieldnames{n_subsystem}};
+    n_subsystem_mass_frac = get_dof_value(lineage, n_gen, dof);
+    sum_mass_frac = sum_mass_frac + n_subsystem_mass_frac;
+    z_plt3 = [prev_sum_mass_frac, sum_mass_frac];
+    graphic_handle = plot3(x_plt3, y_plt3, z_plt3, "linestyle", line_type, "color", line_colors(n_subsystem,:), "linewidth", line_width);
+    graphics_handles = [graphics_handles, graphic_handle];
+    prev_sum_mass_frac = sum_mass_frac;
   end
 end
 
