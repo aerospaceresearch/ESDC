18764d6b554e4870aa50f0a7d5c1e781098431b6
AstroEngy <ehresmann.manfred@gmail.com>
2019-08-23 15:37:38 +0200

    Add files via upload
    Updated propulsion system scaling for new thruster mass scaling function call arguments

--- Code/Analysis/mass_budget_propulsion.m
+++ Code/Analysis/mass_budget_propulsion.m
@@ -1,11 +1,11 @@
-function [mass_propulsion] = mass_budget_propulsion(data)
+function [mass_propulsion] = mass_budget_propulsion(data,db_data)
   %generates masses of the implemented system and calculates the mass total
   
   mass_propulsion = struct();
 
   mass_propulsion.propellant = m_scale_propellant(data.mass, data.dv, data.c_e);
   mass_propulsion.tank       = m_scale_tank(mass_propulsion.propellant, data.propellant); 
-  mass_propulsion.thruster   = m_scale_thruster(data.power_thruster, data.propulsion_system);
+  mass_propulsion.thruster   = m_scale_thruster(data.power_thruster, data.propulsion_system, data.propellant, db_data);
   
   mass_propulsion.PPU        = m_scale_PPU(data.power_thruster, data.eff_PPU, data.propulsion_system);
   mass_propulsion.structure  = m_scale_structure(data.propulsion_system);
