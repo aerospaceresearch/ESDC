8417f1643439362e34c6056febb98112dc3a2499
AstroEngy <ehresmann.manfred@gmail.com>
2019-08-23 16:48:37 +0200

    Add files via upload
    Full generic thruster mass scaling function

--- Code/Scaling/m_scale_thruster.m
+++ Code/Scaling/m_scale_thruster.m
@@ -1,55 +1,64 @@
-function [m_Thruster_out] = m_scale_thruster(P_thruster, type)
-t_1=now;
-m_0_set = [];
-slope_set = [];
+function [m_thruster] = m_scale_thruster(P_thruster, propulsion_type, propellant, db_data)
+% Generates thruster mass estimations for set propulsion types, with predetermined propellants and total supplied power from data base2dec
 
-for i=1:size(P_thruster,2)
-    
-    if strcmp(type,'arcjet')
-    
-        if P_thruster(i) <= 300
-            m_0     = 0.3;        % Velarc mass
-            slope   = 0;        % constant for P <300 W
-        end
-        if ((P_thruster(i)>= 300) && (P_thruster(i) < 1500))
-            m_0     = 0.2;        % ATOS/ARTUS 0.7 kg at 1500 W
-            slope   = 0.00033333333333333333;
+%Generate appropriate filename
+filename = strcat("Database/Scaling/scaling_data_mass_to_power_",propulsion_type,"_propellant_",propellant, ".csv");
 
-        end
-        if ((P_thruster(i) >= 1500) && (P_thruster(i) < 10000))
-            m_0     = 0.4882352941176471;        % MARC
-            slope   = 0.00014117647058823529;
-        end
+%Use lookup file when available
+if exist(filename)
+  data = dlmread(filename,",");
+  
+else
+  %Collect db_data for case, sort, pad out and write to file.
 
-        if P_thruster(i) >= 10000
-            m_0     = 0.0196581196581199205;        % HIPARC
-            slope   = 0.00018803418803418803;  
-        end
-    m_0_set = [m_0_set m_0];
-    slope_set = [slope_set slope];
-    elseif strcmp(type,'gridionthruster')       % http://www.space-propulsion.com/brochures/electric-propulsion/electric-propulsion-thrusters.pdf
-            
-        if P_thruster(i) <= 50                            % RIT μX
-            m_0     =0.44;    
-            slope   =0;             
-        end
-        
-        if ((P_thruster(i)>= 50) && (P_thruster(i) < 2000))     %RIT 10 EVO
-            m_0     =0.027;    
-            slope   =0.0034;       
-        end
-        
-        if P_thruster(i) >= 2000                          % RIT 2X
-            m_0     =0.027;    
-            slope   =0.0034;    
-        end
-    m_0_set = [m_0_set m_0];
-    slope_set = [slope_set slope];
-    else
-        disp('This type of thruster not yet implemented');
-    end
+  %Define arrays
+  arr_pow = [];
+  arr_mass = [];
+  
+  %Walk db for cases
+  for i=1:numel(db_data.reference_data.propulsion_system.(propulsion_type).thruster)
+    %Shortcut to relevant data
+    data_point =db_data.reference_data.propulsion_system.(propulsion_type).thruster{1,i};
+    %test for relevant field existance
+    if isfield(data_point, "mass") && isfield(data_point, "efficiency") && isfield(data_point, "thrust") &&  isfield(data_point, "c_e")
+      %test for propellant case - changes efficiency and power demand
+      if strcmp(propellant, data_point.propellant)
+  
+          %Short descriptors
+          eta = data_point.efficiency;
+          F = data_point.thrust;
+          c_e = data_point.c_e;
+          
+          %calculate and colelct data
+          arr_pow = [arr_pow F*c_e/(2*eta)];
+          arr_mass = [arr_mass data_point.mass];
+      endif
+    endif
+  end
+  
+  %Sort data to power 
+  [sorted_pow idx] = sort(arr_pow);
+  
+  %arranged related data
+  for i=1:numel(arr_pow)
+   sorted_mass(i) = arr_mass(idx(i));
+  endfor
+  
+  %Pad cases for P = 0 W for minimal mass of lightest known piece of hardware
+  sorted_pow = [0 sorted_pow];
+  sorted_mass = [sorted_mass(1) sorted_mass];
+  data = [sorted_pow; sorted_mass];
+  
+  %write file  
+  dlmwrite(filename, data, ",");
 end
-    m_Thruster_out = m_0_set + slope_set.*P_thruster;
-    
-    
+
+
+%Interpolate for known data, extrapolate beyond.
+if P_thruster<=data(1,end)
+  m_thruster = interp1(data(1,:),data(2,:),P_thruster,"linear");
+else
+  m_thruster = interp1([data(1,1) data(1,end)],[data(2,1) data(2,end)],P_thruster,"extrap");
+end
+
 end
