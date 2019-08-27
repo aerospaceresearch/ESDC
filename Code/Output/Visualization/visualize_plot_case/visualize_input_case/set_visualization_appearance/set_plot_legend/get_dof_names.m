4055c55e635aad957910d9659433963dd7148f2d
AstroEngy <ehresmann.manfred@gmail.com>
2019-08-27 15:41:49 +0200

    Update get_dof_names.m
    type line 6 pot to plot

--- Code/Output/Visualization/visualize_plot_case/visualize_input_case/set_visualization_appearance/set_plot_legend/get_dof_names.m
+++ Code/Output/Visualization/visualize_plot_case/visualize_input_case/set_visualization_appearance/set_plot_legend/get_dof_names.m
@@ -3,7 +3,7 @@ function dof_names = get_dof_names(plot_case, plot_dof)
     if plot_case.appearance.custom_display_names.(plot_dof).active
       dof_names = plot_case.appearance.custom_display_names.(plot_dof).name;
     else
-      dof_names = pot_case.(plot_dof).dof_values;
+      dof_names = plot_case.(plot_dof).dof_values;
     end
     if strcmp(plot_dof, "line_color") || strcmp(plot_dof, "marker_color")
       dof_names = strcat(dof_names, sprintf(" [%.2f,%.2f]", plot_case.(plot_dof).min_value, plot_case.(plot_dof).max_value));
