98c97278120f02122e78114ee67ff8db78f2a21e
s-themis <themistos@auth.gr>
2019-08-28 13:47:50 +0300

    fixed seed point bug

--- Code/Output/Visualization/visualize_plot_case/visualize_input_case/visualize_lineage/visualize_lineage_2d.m
+++ Code/Output/Visualization/visualize_plot_case/visualize_input_case/visualize_lineage/visualize_lineage_2d.m
@@ -1,9 +1,7 @@
 function graphics_handles = visualize_lineage_2d(plot_case, lineage)
   num_gens = numel(lineage);
-  if is_scatter_active(plot_case) && are_seed_points_active(plot_case)
-    n_gen = 1;
-    graphics_handles{n_gen} = plot_seed_point_2d(plot_case, lineage, n_gen);
-  end
+  n_gen = 1;
+  graphics_handles{n_gen} = plot_seed_point_2d(plot_case, lineage, n_gen);
   for n_gen = 2:num_gens
     if is_mutation_successful(lineage, n_gen) || are_failed_mutations_active(plot_case)
       graphics_handles{n_gen} = plot_generation_2d(plot_case, lineage, n_gen);
