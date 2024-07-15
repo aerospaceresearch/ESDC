af7b4e66915122f25c16461c131e3352f68fddc2
AstroEngy <ehresmann.manfred@gmail.com>
2024-07-15 11:24:31 +0200

    Improved main function for better readibility and maintainability

--- ESDC.m
+++ ESDC.m
@@ -1,6 +1,29 @@
-%main function of the ESDC program. The tools allows for generic spacecraft design based on correlations and application of an evolutionary algorithm.
- %https://standards.nasa.gov/standard/gsfc/gsfc-std-1000 gold rules for margins and more
-function [] =ESDC(runID)
+% ESDC: Evolutionary Spacecraft Design Code
+% 
+% This function serves as the main entry point for the ESDC program. It facilitates
+% the generic design of spacecraft using correlations and applies evolutionary optimization
+%
+% Parameters:
+%   runID (optional): Identifier for the current run of the program. If not provided, defaults to 0.
+%
+% Functionality:
+%   1. Initializes the environment and adds necessary paths for the relevant folders.
+%   2. Starts the program and logs the initial status.
+%   3. Updates the scaling database if needed.
+%   4. Processes the input files for the current simulation.
+%   5. Executes the evolutionary algorithm to solve the design problem.
+%   6. Generates the output in XML format.
+%   7. Logs the completion status and total execution time.
+%   8. Handles any exceptions, logs error messages, and terminates the program if an error occurs.
+%
+% Usage:
+%   ESDC(runID)
+%
+% Example:
+%   ESDC(1)  % Runs the ESDC program with runID set to 1
+%   ESDC()   % Runs the ESDC program with the default runID (0)
+
+function ESDC(runID)
 if nargin < 1
   runID = 0;
 end
@@ -26,7 +49,7 @@ startup();                              % Display startup messages, licenses etc
 appendToLogFileDCEP('DCEP_STATUS: RUNNING_0%',1,runID)
 
 %Update Scaling Data Base
-force_db_update = 0 ;
+force_db_update = 0 ;                                  % Set this for debugging and data base scaling law tests
 update_scaling_model(force_db_update);                 % Checks for changes in the data bases and derives changed scaling laws
 
 %Input
@@ -42,7 +65,7 @@ evolution_time(evolutionStartTime);                % Calculate the time for the 
 %Output
 output_XML_generation(input, db_data, config, evolution_data,runID);
 
-%Visual Output
+%Visual Output   - old code needs revision and adaption
 %disp('Visual production ')
 %visualization(evolution_data, config);
 %disp('Visual production complete')
@@ -50,7 +73,8 @@ output_XML_generation(input, db_data, config, evolution_data,runID);
 
 %End
 disp('ESDC complete')
-eval_total_time(startTime, t_end=now)
+completionTime=now;
+eval_total_time(startTime, completionTime)
 
 appendToLogFileDCEP('DCEP_STATUS: FINISHED',0,runID)
 
